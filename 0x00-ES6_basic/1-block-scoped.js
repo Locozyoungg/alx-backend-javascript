4d4c5894c8827a895c201d71060ffb2f79a84750
Locozyoungg <collaustine27@gmail.com>
2024-06-25 01:44:15 +0300

    block-fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;  // Initialize task as false using let
-  const task2 = true;  // Initialize task2 as true using const
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Define task again within the block scope using let
-    const task2 = false;  // Define task2 again within the block scope using const
-    return [task, task2];  // Return the block-scoped variables
+    /*eslint-disable */
+    const task = true;
+    const task2 = false;
   }
 
-  return [task, task2];  // Return the original variables
+  return [task, task2];
 }
-
--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,6 +1,4 @@
-// 1-main.js
 import taskBlock from './1-block-scoped.js';
 
-console.log(taskBlock(true));  // Should output: [ true, true ]
-console.log(taskBlock(false)); // Should output: [ false, true ]
-
+console.log(taskBlock(true));
+console.log(taskBlock(false));
