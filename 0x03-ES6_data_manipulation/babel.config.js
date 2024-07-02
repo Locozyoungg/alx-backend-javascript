54c342b74bce8577026d44a417ce2db8b19a1b3d
Locozyoungg <collaustine27@gmail.com>
2024-07-02 13:16:03 +0300

    list_students

--- 0x03-ES6_data_manipulation/babel.config.js
+++ 0x03-ES6_data_manipulation/babel.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+echo "module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }]
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '12'
+        }
+      }
+    ]
   ]
-};
+};" > babel.config.js
 
